38e9e814a1a97823cf3ecace112f31c5d7f17fda
DY-2001 <89105143+DY-2001@users.noreply.github.com>
2023-07-26 21:13:32 +0530

    integrated firebase

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB_bFG0TTQqMeRmif5qE60XKK4mI9keJHE",
+  authDomain: "coin-world-b14ec.firebaseapp.com",
+  projectId: "coin-world-b14ec",
+  storageBucket: "coin-world-b14ec.appspot.com",
+  messagingSenderId: "689227078619",
+  appId: "1:689227078619:web:0eb70d18813899ada9a5fe",
+};
+
+export default firebaseConfig;
